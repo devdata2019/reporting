96d5afe49687e02e9ba4b866432ede4573bba036
ejhb <bonurajosh13@gmail.com>
2019-12-18 16:56:34 +0100

    update_cleanup

--- py.py
+++
@@ -1,9 +0,0 @@
-def temperature(day, month, temp):
-    print(day + "." + month + " ", end="")
-    print("   "*(temp+5) + "-", end="")
-    print()
-def temperatureaxis():
-    print("      ", end="")
-    for i in range(-5,16):
-        print("{:02d} ".format(i), end="")
-    print()
